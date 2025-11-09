158e462057982b3b227cd0e265edbbb4e415c217
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2025-11-10 02:07:18 +0600

    feat: add old decisions for 2 task

--- src/queue.js
+++ src/queue.js
@@ -12,21 +12,42 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 class Queue {
+  constructor() {
+    this.head = null;  // создаем значение для  начала списка
+    this.tail = null;  //создаем значение для конца списка
+ }
+  
   getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.head; //выводим список с самого начала
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    
+    const node = new ListNode(value);
+
+      if (!this.head || !this.tail) {
+        this.head= node;//обработка случая если не было ничего
+        this.tail = node;
+      } else {
+        this.tail.next =node;// поправляем конец списка
+        this.tail = node;
+      }
+      return this;
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    let headDot = this.head; //первый эл
+    this.head = this.head.next;//убираем его из списка
+    return headDot.value;//выводим то что удалили
   }
+
 }
 
 module.exports = {
